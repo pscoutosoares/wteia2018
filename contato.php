27262068a8073f232d05a97efa9e84f86ea619e6
pedro <pscoutosoares@gmail.com>
2018-03-01 22:20:36 -0300

    alterações

--- contato.php
+++ contato.php
@@ -1,3 +1,36 @@
+<?
+
+$nome=$_POST['nome'];
+
+$email=$_POST['email'];
+
+$titulo=$_POST['titulo'];
+
+$texto=$_POST['texto'];
+
+
+$Destinatario="wteia2018@gmail.com";
+
+
+$Titulo="[Contato WTEIA] - $titulo";
+
+
+$mensagem1="
+
+Uma mensagem vinda do site !
+
+Algum vistante mandou essa mensagem pelo site.
+
+Nome: $nome
+
+Email: $email
+
+Mensagem: $texto";
+
+
+mail("$Destinatario","$Titulo", "$mensagem1","From:$email");
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -30,7 +63,7 @@
       });
     </script>
     <style>
-    .ul {
+    ul {
       font-family: exo_medium;
     }
     
@@ -41,7 +74,7 @@
       <div class="header-wrapper">
         <img src="img/banner_site.png">
       </div>
-<div class="menu">
+      <div class="menu">
         <div class="navbar">
           <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -96,39 +129,28 @@
       <div class="container">
         <div class="row">
           <div class="col-md-12 textPost">
-            <h1>O evento</h1>
-            <p>
-              Garanhuns é um município situado no Agreste Pernambucano,
-              distante cerca de 230 km da capital pernambucana, Recife. Tem uma
-              população de aproximadamente 137.810 habitantes, segundo dados de 2016
-              do IBGE, sendo o nono município mais populoso do estado, o terceiro do
-              interior e o segundo da região do Agreste. A cidade de Garanhuns é o centro
-              mais diversificado do agreste meridional, sendo polo de 32 municípios,
-              concentra em seu entorno cerca de 1.066.000 habitantes, o que a torna
-              também um centro regional de saúde e educação. Em relação à educação, a
-              Universidade Federal Rural de Pernambuco - UFRPE (Unidade Acadêmica
-              de Garanhuns - UAG), a Universidade de Pernambuco - UPE (Campus
-              Garanhuns), a Autarquia de Ensino Superior de Garanhuns - AESGA e o
-              Instituto Federal de Pernambuco(IFPE) oferecem cursos de graduação e
-              pós-graduação. Pelo seu diversificado comércio e oferta de serviços,
-              Garanhuns tem no turismo um importante fator de geração de empregos,
-              renda e desenvolvimento. A cidade tem uma rica cultura de turismo, dispõe
-              de uma grande rede de empresas prestadoras de serviços e de hotéis
-              direcionados ao turismo. Anualmente a cidade sedia, no mês de Julho, o
-              Festival de Inverno de Garanhuns, maior evento multicultural da América
-              Latina que atrai milhares de turistas de todo o mundo.
-            </p>
-            <p>
-              Fontes:
-              <br><a href="www.garanhuns.pe.gov.br">https://www.garanhuns.pe.gov.br</a>
-              <br><a href="https://pt.wikipedia.org/wiki/Garanhuns">https://pt.wikipedia.org/wiki/Garanhuns</a> 
+            
+            <p><p>
+			<center>
+			<h3>
+			Agradecemos o seu contato! <br>
+			Responderemos em breve! <br>
+			
+			Equipe WTEIA 2018.<br>
+			
+			</h3>
+					
+			</center>
+			
+			
+			
             </p>
           </div>
         </div>
       </div>
-    </div>
+   </div>
  <!-- Redes sociais -->
-    <div class="row rodape">
+      <div class="row rodape">
       <div class="col-sm-12">
         <div class="col-sm-4 realizacao" style="padding-top: 20px">
             <img src="img/rodape/0.png" style="width: 5%">
@@ -167,7 +189,6 @@
           </ul>
         </div>
       </div>
-   
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
